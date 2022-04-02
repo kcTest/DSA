fd5172ada0ed6f8895b6d6fce495337c0d842d86
kcTest <763336180@qq.com>
2022-04-02 20:20:35 +0800

    SmallSum

--- src/com/zkc/MergeSort.java
+++ src/com/zkc/MergeSort.java
@@ -54,39 +54,35 @@ public class MergeSort {
 	 * 将另一序列剩下的所有元素直接复制到合并序列尾。
 	 */
 	private static void mergeSort(int[] arr, int left, int right, int[] tempArr) {
-		if (left < right) {
-			int mid = left + ((right - left) >> 1);
-			mergeSort(arr, left, mid, tempArr);
-			mergeSort(arr, mid + 1, right, tempArr);
-			merge(arr, left, mid, right, tempArr);
+		if (left == right) {
+			return;
 		}
+		int mid = left + ((right - left) >> 1);
+		mergeSort(arr, left, mid, tempArr);
+		mergeSort(arr, mid + 1, right, tempArr);
+		merge(arr, left, mid, right, tempArr);
 	}
 	
 	private static void merge(int[] arr, int left, int mid, int right, int[] temp) {
-		int i = left;
-		int j = mid + 1;
+		int l = left;
+		int r = mid + 1;
 		//临时数组存储相邻序列元素排序结果  
 		int t = 0;
 		//升序  较小者放入临时数组 且该侧继续向右移动下标获取新元素与另一侧元素对比。
-		while (i <= mid && j <= right) {
-			if (arr[j] < arr[i]) {
-				temp[t++] = arr[j++];
-			} else {
-				temp[t++] = arr[i++];
-			}
+		while (l <= mid && r <= right) {
+			temp[t++] = arr[r] > arr[l] ? arr[l++] : arr[r++];
 		}
 		
 		//两个数组比较完成时其中一个数组元素全部放置完成下标将超出其索引范围，另一数组会有剩余元素无法做对比，将这部分剩余元素全部放入临时数组末尾
 		//左边全部放完时i=mid+1
-		while (i <= mid) {
-			temp[t++] = arr[i++];
+		while (l <= mid) {
+			temp[t++] = arr[l++];
 		}
-		while (j <= right) {
-			temp[t++] = arr[j++];
+		while (r <= right) {
+			temp[t++] = arr[r++];
 		}
-		
+		//从头取 长度由传入的起始位置得出
 		t = 0;
-		
 		//将排好序的左右俩部分序列元素再复制到原数组的指定范围内
 		while (left <= right) {
 			arr[left++] = temp[t++];
---
+++ src/com/zkc/SmallSum.java
@@ -0,0 +1,68 @@
+package com.zkc;
+
+import com.zkc.utils.ArrayUtils;
+
+/**
+ * 求最小和
+ * 对数组中每一个数求：左侧所有小于当前位置数的数的和
+ * 每个数求得的值再求和
+ */
+public class SmallSum {
+	
+	public static void main(String[] args) {
+		int[] arr = ArrayUtils.getArray(7, 20);
+		printArr(arr);
+		System.out.println();
+		System.out.println("===================");
+		int[] tempArr = new int[arr.length];
+		System.out.println(mergeSort(arr, 0, arr.length - 1, tempArr));
+	}
+	
+	private static void printArr(int[] arr) {
+		for (int j : arr) {
+			System.out.printf("%d ", j);
+		}
+	}
+	
+	private static int mergeSort(int[] arr, int left, int right, int[] tempArr) {
+		if (left == right) {
+			return 0;
+		}
+		int mid = left + ((right - left) >> 1);
+		return mergeSort(arr, left, mid, tempArr) + mergeSort(arr, mid + 1, right, tempArr) + merge(arr, left, mid, right, tempArr);
+	}
+	
+	private static int merge(int[] arr, int left, int mid, int right, int[] temp) {
+		int l = left;
+		int r = mid + 1;
+		int t = 0;
+		int sum = 0;
+		
+		while (l <= mid && r <= right) {
+			//右侧比左侧大的情况下求和 ，否则不求和 ，因为相等的情况下无法确定在右侧有几个比左侧当前数大的数
+			if (arr[r] > arr[l]) {
+				//如果左侧比右侧小 右侧当前索引及之后的数都比左侧大。右侧比左侧当前数大的数的个数乘以左侧当前数
+				int current = arr[l++];
+				temp[t++] = current;
+				sum += (right - r + 1) * current;
+				
+			} else {
+				temp[t++] = arr[r++];
+			}
+		}
+		
+		while (l <= mid) {
+			temp[t++] = arr[l++];
+		}
+		while (r <= right) {
+			temp[t++] = arr[r++];
+		}
+		t = 0;
+		while (left <= right) {
+			arr[left++] = temp[t++];
+		}
+		
+		return sum;
+	}
+	
+}
