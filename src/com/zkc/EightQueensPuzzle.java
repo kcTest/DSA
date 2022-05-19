dbdbd9f6b6c4205531727176a4e8ddd032bf7090
kcTest <763336180@qq.com>
2022-05-19 23:54:51 +0800

    solveEightQueue

--- src/com/zkc/EightQueensPuzzle.java
+++ src/com/zkc/EightQueensPuzzle.java
@@ -6,16 +6,14 @@ public class EightQueensPuzzle {
 	
 	public static void main(String[] args) {
 //		int dim=(int) (Math.random() * 9);
-		int dim = 4;
+		int dim = 5;
 		List<List<String>> result = solveEightQueue(dim);
 		StringBuilder sb = new StringBuilder();
 		sb.append("[\n");
-		for (int j = 0; j < result.size(); j++) {
+		for (List<String> strings : result) {
 			sb.append("[");
-			List<String> rowStrLst = result.get(j);
-			for (int i = 0; i < rowStrLst.size(); i++) {
-				String str = rowStrLst.get(i);
-				sb.append(str);
+			for (String str : strings) {
+				sb.append(str).append(" ");
 			}
 			sb.append("]\n");
 		}
@@ -28,27 +26,25 @@ public class EightQueensPuzzle {
 		List<List<String>> result = new ArrayList<>();
 		RowCell[][] rowArr = new RowCell[dim][dim];
 		for (int i = 0; i < dim; i++) {
-			HashSet<RowCell> row = new HashSet<>();
 			for (int j = 0; j < dim; j++) {
 				RowCell rowCell = new RowCell(i, j);
 				rowArr[i][j] = rowCell;
-				row.add(rowCell);
 			}
 		}
 		Set<RowCell> invalidCells = new HashSet<>();
-		Map<Integer, RowCell> validCellPerRowMap = new HashMap<>();
-		solveEightQueueSub(result, rowArr, 0, dim, validCellPerRowMap, invalidCells);
+		List<RowCell> validCellPerRow = new ArrayList<>();
+		solveEightQueueSub(result, rowArr, 0, validCellPerRow, invalidCells);
 		return result;
 	}
 	
 	
-	private static void solveEightQueueSub(List<List<String>> result, RowCell[][] rowArr, int curRowIdx, int dim, Map<Integer, RowCell> validCellPerRowMap, Set<RowCell> invalidCells) {
-		if (curRowIdx == dim) {
+	private static void solveEightQueueSub(List<List<String>> result, RowCell[][] rowArr, int curRowIdx, List<RowCell> validCellPerRow, Set<RowCell> invalidCells) {
+		if (curRowIdx == rowArr.length) {
 			List<String> rowStrLst = new ArrayList<>();
-			for (Map.Entry<Integer, RowCell> entry : validCellPerRowMap.entrySet()) {
+			for (RowCell cell : validCellPerRow) {
 				StringBuilder sb = new StringBuilder();
-				for (int i = 0; i < dim; i++) {
-					if (i == entry.getValue().colIdx) {
+				for (int i = 0; i < rowArr.length; i++) {
+					if (i == cell.colIdx) {
 						sb.append("Q");
 					} else {
 						sb.append(".");
@@ -60,78 +56,60 @@ public class EightQueensPuzzle {
 			return;
 		}
 		for (RowCell curRowCell : rowArr[curRowIdx]) {
-			int rowIdx = curRowCell.rowIdx;
-			for (int i = rowIdx; i < dim; i++) {
-				if (validCellPerRowMap.containsKey(i)) {
-					Set<RowCell> crossCellSet = validCellPerRowMap.get(i).crossCellSet;
-					validCellPerRowMap.remove(i);
+			for (int i = rowArr.length - 1; i >= curRowCell.rowIdx; i--) {
+				if (validCellPerRow.size() > i) {
+					Set<RowCell> crossCellSet = validCellPerRow.get(i).crossCellSet;
+					validCellPerRow.remove(i);
 					for (RowCell rowCell : crossCellSet) {
 						invalidCells.remove(rowCell);
 						rowCell.valid = true;
 					}
-					crossCellSet.clear();
 				}
 			}
 			if (!curRowCell.valid) {
 				continue;
 			}
-			Set<RowCell> crossedCellSet = getCrossedCell(curRowCell, dim, rowArr, invalidCells);
-			curRowCell.crossCellSet = crossedCellSet;
-			for (RowCell rowCell : crossedCellSet) {
-				rowCell.valid = false;
-			}
-			validCellPerRowMap.put(rowIdx, curRowCell);
-			solveEightQueueSub(result, rowArr, curRowIdx + 1, dim, validCellPerRowMap, invalidCells);
+			handleCrossedCell(curRowCell, rowArr, invalidCells);
+			validCellPerRow.add(curRowCell.rowIdx, curRowCell);
+			solveEightQueueSub(result, rowArr, curRowIdx + 1, validCellPerRow, invalidCells);
 		}
 	}
 	
-	private static Set<RowCell> getCrossedCell(RowCell cell, int dim, RowCell[][] rowArr, Set<RowCell> invalidCells) {
+	private static void handleCrossedCell(RowCell curRowCell, RowCell[][] rowArr, Set<RowCell> invalidCells) {
 		Set<RowCell> linkedCell = new HashSet<>();
-		int rowIdx = cell.rowIdx;
-		int colIdx = cell.colIdx;
-		for (int i = 0; i < dim; i++) {
-			RowCell sameRow = rowArr[rowIdx][i];
-			if (sameRow != cell && !invalidCells.contains(sameRow)) {
-				linkedCell.add(sameRow);
-				invalidCells.add(sameRow);
-			}
-			RowCell sameCol = rowArr[i][colIdx];
-			if (sameCol != cell && !invalidCells.contains(sameCol)) {
-				linkedCell.add(sameCol);
-				invalidCells.add(sameCol);
-			}
-			if (rowIdx + i >= 0 && rowIdx + i < dim && colIdx + i >= 0 && colIdx + i < dim) {
-				RowCell diagonalBottomRight = rowArr[rowIdx + i][colIdx + i];
-				if (diagonalBottomRight != cell && !invalidCells.contains(diagonalBottomRight)) {
-					linkedCell.add(diagonalBottomRight);
-					invalidCells.add(diagonalBottomRight);
+		for (int i = 0; i < rowArr.length; i++) {
+			if (i > curRowCell.rowIdx) {
+				RowCell sameCol = rowArr[i][curRowCell.colIdx];
+				if (sameCol != curRowCell && !invalidCells.contains(sameCol)) {
+					sameCol.valid = false;
+					linkedCell.add(sameCol);
+					invalidCells.add(sameCol);
 				}
 			}
-			if (rowIdx - i >= 0 && rowIdx - i < dim && colIdx - i >= 0 && colIdx - i < dim) {
-				RowCell diagonalTopLeft = rowArr[rowIdx - i][colIdx - i];
-				if (diagonalTopLeft != cell && !invalidCells.contains(diagonalTopLeft)) {
-					linkedCell.add(diagonalTopLeft);
-					invalidCells.add(diagonalTopLeft);
+			int nextRowIdx = curRowCell.rowIdx + i;
+			if (nextRowIdx >= 0 && nextRowIdx < rowArr.length) {
+				int nextColIdx = curRowCell.colIdx + i;
+				if (nextColIdx >= 0 && nextColIdx < rowArr.length) {
+					RowCell diagonalBottomRight = rowArr[nextRowIdx][nextColIdx];
+					if (diagonalBottomRight != curRowCell && !invalidCells.contains(diagonalBottomRight)) {
+						diagonalBottomRight.valid = false;
+						linkedCell.add(diagonalBottomRight);
+						invalidCells.add(diagonalBottomRight);
+					}
 				}
-			}
-			if (rowIdx - i >= 0 && rowIdx - i < dim && colIdx + i >= 0 && colIdx + i < dim) {
-				RowCell diagonalTopRight = rowArr[rowIdx - i][colIdx + i];
-				if (diagonalTopRight != cell && !invalidCells.contains(diagonalTopRight)) {
-					linkedCell.add(diagonalTopRight);
-					invalidCells.add(diagonalTopRight);
-				}
-			}
-			if (rowIdx + i >= 0 && rowIdx + i < dim && colIdx - i >= 0 && colIdx - i < dim) {
-				RowCell diagonalBottomLeft = rowArr[rowIdx + i][colIdx - i];
-				if (diagonalBottomLeft != cell && !invalidCells.contains(diagonalBottomLeft)) {
-					linkedCell.add(diagonalBottomLeft);
-					invalidCells.add(diagonalBottomLeft);
-					
+				int prevColIdx = curRowCell.colIdx - i;
+				if (prevColIdx >= 0 && prevColIdx < rowArr.length) {
+					RowCell diagonalBottomLeft = rowArr[nextRowIdx][prevColIdx];
+					if (diagonalBottomLeft != curRowCell && !invalidCells.contains(diagonalBottomLeft)) {
+						diagonalBottomLeft.valid = false;
+						linkedCell.add(diagonalBottomLeft);
+						invalidCells.add(diagonalBottomLeft);
+						
+					}
 				}
 			}
 		}
-		
-		return linkedCell;
+		curRowCell.crossCellSet = linkedCell;
 	}
 	
 	private static class RowCell {
