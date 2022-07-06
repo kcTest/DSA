78838c2dcd850ad1d8bfc51ce6f5f5a4bcc6a5ac
kcTest <763336180@qq.com>
2022-07-06 18:24:49 +0800

    NumEqualMinSteps

--- src/com/zkc/NumEqualMinSteps.java
+++ src/com/zkc/NumEqualMinSteps.java
@@ -24,8 +24,7 @@ public class NumEqualMinSteps {
 	 * 如果第i轮ab相等 第i轮分配出去的数为x y的总和为sum=（1+i)*i/2；所以 x+y=sum,y-x=d ---> x=(sum-d)/2;
 	 * 分配给a的数有可能为0，所以(sum-d)/2>=0--->sum>=d;因为添加给a或b的数之和都为整数,所以sum-d要为偶数。
 	 * 所以i只需要从1开始遍历  尝试计算((1+i)*i/2),遇到的第一个大于等于d并且为偶数的sum停止， 此时的i为返回结果。
-	 * 为了加快遍历速度 i从1开始翻倍计算。如果得到了一个偶数，则最终结果i会在翻倍前的位置和当前位置之间。
-	 * 此时可以用二分法找到商为偶数的最小的i返回。
+	 * 为了加快遍历速度 i从1开始翻倍计算。
 	 */
 	private static int getMinSteps(int a, int b) {
 		if (a == b) {
@@ -38,7 +37,7 @@ public class NumEqualMinSteps {
 		}
 		int d = a - b;
 		int i = 1;
-		//先找到快速找到一个刚开始满足sum>=d的位置 从该位置遍历
+		//先快速找到一个刚开始满足sum>=d的位置 从该位置向后遍历检查是否满足sum>=d 并且sum-d为偶数
 		while (true) {
 			int sum0i = (((1 + i) * i) >> 1);
 			if (sum0i >= d) {
