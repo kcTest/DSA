99f640fdf402a5b2dd58258269e627bcb37672b0
kcTest <763336180@qq.com>
2022-07-21 11:23:45 +0800

    isPowerOfThree

--- src/com/zkc/BitwiseOperation/IsPowerOfN.java
+++ src/com/zkc/BitwiseOperation/IsPowerOfN.java
@@ -3,8 +3,11 @@ package com.zkc.BitwiseOperation;
 public class IsPowerOfN {
 	
 	public static void main(String[] args) {
-		System.out.println(isPowerOfTwo(32));
-		System.out.println(isPowerOfFour(64));
+//		System.out.println(isPowerOfTwo(32));
+//		System.out.println(isPowerOfFour(64));
+		for (int i = 0; i < 10; i++) {
+			System.out.println(isPowerOfThree(Integer.MIN_VALUE));
+		}
 	}
 	
 	private static boolean isPowerOfTwo(int x) {
@@ -21,6 +24,40 @@ public class IsPowerOfN {
 		}
 		//只包含一个1并且出现在0 2 4 8 位置
 		return (x & (x - 1)) == 0 && (x & 0x55555555) != 0;
+	}
+	
+	private static boolean isPowerOfThree(int n) {
+		if (n <= 0) {
+			return false;
+		}
+		if (n == 1 || n == 3) {
+			return true;
+		}
+		double curVal;
+		int zs = 1;
+		do {
+			curVal = Math.pow(3, zs << 1);
+			if (curVal == n) {
+				return true;
+			} else if (curVal > n) {
+				int low = zs;
+				int high = zs << 1;
+				while (low <= high) {
+					int mid = low + ((high - low) >> 1);
+					curVal = Math.pow(3, mid);
+					if (curVal < n) {
+						low = mid + 1;
+					} else if (curVal == n) {
+						return true;
+					} else {
+						high = mid - 1;
+					}
+				}
+				return false;
+			}
+			zs <<= 1;
+		} while (curVal < n);
+		return false;
 	}
 	
 	private static int log2(int N) {
